6432a35a858a137e758d6504878554425dc9e93d
Parsley Njoroge <147031484+Parsley0@users.noreply.github.com>
2025-11-23 19:59:33 +0300

    initial commit

--- answers.sql
+++ answers.sql
@@ -1,1 +1,18 @@
 
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    fullName VARCHAR(100),
+    age INT
+);
+
+
+INSERT INTO student (id, fullName, age) VALUES 
+(1, 'Alice Johnson', 19),
+(2, 'Bob Smith', 22),
+(3, 'Charlie Davis', 21);
+
+
+
+UPDATE student
+SET age = 20
+WHERE id = 2;
